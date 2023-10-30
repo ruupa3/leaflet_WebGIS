7b246c7e09acd78068ef4e8091350d2129584bba
Satarupa Mitra <119962586+ruupa3@users.noreply.github.com>
2023-10-30 14:37:33 +0530

    Add files via upload

--- leaflet.js
+++ leaflet.js
@@ -1,4 +1,5 @@
 
+//Defining the map variable
 var map = L.map('map', {
   fullscreenControl: true,
   fullscreenControlOptions: {
@@ -7,116 +8,57 @@ var map = L.map('map', {
   }
 }).setView([23.680223, 87.269842], 12);
 
+//Assigning the OpenStreetMap to a variable
 var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 20,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 });
 
-var url = 'http://localhost:8080/geoserver/coalrr/wms';
-
+//Assigning the WMS layers 
 var wmsLayers = L.tileLayer.wms("http://localhost:8080/geoserver/coalrr/wms", {
-    layers: 'coalrr:jhajra_owner_join, coalrr:sonepur_owner_join',
+    layers: 'coalrr:kumarkhala_owner_join_modified, coalrr:benibandei_owner_join_modified',
     format: 'image/png',
     transparent: true,
 });
 
+//Adding the OSM layer to the map
 osm.addTo(map);
+
+//Adding the WMS layer to the map
 wmsLayers.addTo(map);
 
-// map.on('click', function (e) {
-//     getFeatureInfo(e.u_plot_no);
-// });
-
-// add a toolbar
-// var toolbar = L.Toolbar();
-// toolbar.addToolbar(map);
-
-//add map scale
+//add a scale to the map
 var MapScale = L.control.scale(); 
 MapScale.addTo(map);
 
-//full screen map view JavaScript
-// var FullScreen = document.getElementById('map');
-// function fullScreenView()
-// {
-//     FullScreen.requestFullscreen();
-// }
-//THIS IS WORKING
-
-//full screen map view Leaflet
-// map.addControl(new L.Control.Fullscreen());
-
-//map coordinate display
+//displaying the coordinate at the cursor position
 map.on('mousemove', function(e){
     // console.log(e)
     $('.coordinate').html(`Lat: ${e.latlng.lat} lng: ${e.latlng.lng}`)
 })
 
-//leaflet layer control ENDING
-
-//Leaflet Search
-//L.Control.geocoder().addTo(MyMap);
+//Latitude-Longitude search in Leaflet
 var SearchControl = L.Control.geocoder();
 SearchControl.addTo(map);
 
-//add marker to map
-// var marker = L.marker ([23.717853, 87.243203]).addTo(MyMap);
-
-//search location by lat/lng
-// $('#search-btn').click(function() {
-//     var latlng = $('#search').val(); //taking the value of search
-//     var latlngArr = latlng.split(','); //to accept the lat-lng separated by comma
-//     var lat = latlngArr[0]; //latitude is the first value
-//     var lng = latlngArr[1]; //longitude is the second value
-//     map.setView([lat, lng], 8); //zoom level 8
-//     marker.setLatLng([lat, lng]); //setting the marker
-// });
-
-//Popup onclick without marker
-// Attach a click event listener to the map
-// map.on('click', function(event) {
-//   var clickedLatLng = event.latlng;
-// //   var clickedFeature = event.feature;
-//   var popupContent = "<p>Clicked coordinates:</p>" + clickedLatLng.lat + "," + clickedLatLng.lng;
-// //   var popup_Content = "<p>Plot No: <p>" + clickedFeature.properties.text;
-// //   // Create a popup at the clicked location
-//   var popup = L.popup()
-//     .setLatLng(clickedLatLng)
-//     .setContent(popupContent)
-//     popup.openOn(map);
-// });
-//THIS IS WORKING
-
-// function getInfo(latlng) {
-//     var params = {
-//         request: 'GetFeatureInfo',
-//         service: 'WMS',
-//         srs: 'EPSG:4326',
-//         styles: '',
-//         transparent: true,
-//         version: '1.1.1',
-//         format: 'image/png',
-//         bbox: map.getBounds().toBBoxString(),
-//         height: map.getSize().y,
-//         width: map.getSize().x,
-//         layers: 'coalrr:jhajra_owner_join, coalrr:sonepur_owner_join',
-//         query_layers: 'coalrr:jhajra_owner_join, coalrr:sonepur_owner_join',
-//         info_format: 'text/html'
-//     };
-//     params[params.version === '1.3.0' ? 'i' : 'x'] = latlng.lng;
-//     params[params.version === '1.3.0' ? 'j' : 'y'] = latlng.lat;
-//     fetch('http://localhost:8080/geoserver/coalrr/wms', {
-//         method: 'GET',
-//         params: params
-//     })
-//     .then(function(response) {
-//         return response.u_plot_no();
-//     })
-//     .then(function(data) {
-//         // Display the retrieved feature info in a popup or any other way you prefer
-//         console.log(data);
-//     });
-// }
+// HomeButtonControl: Start
+var homeControl = L.Control.extend({
+  options: {
+      position: 'topleft' // Adjust the position as needed
+  },
+  onAdd: function (map) {
+      var container = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom');
+  container.innerHTML = 
+  '<a href="#" title="Home" role="button" aria-label="Home" id="home-button"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"/></svg></a>';
+  L.DomEvent.disableClickPropagation(container);
+    return container;
+  }
+});
+map.addControl(new homeControl());
+document.getElementById('home-button').addEventListener('click', function () {
+  map.setView([23.680223, 87.269842], 12); // Replace with your initial coordinates and zoom level
+});
+// HomeButtonControl: End
 
 //Geoserver Web Feature Service
 var jhajraGeoJSON = $.ajax('http://localhost:8080/geoserver/coalrr/wfs',{
@@ -125,7 +67,7 @@ var jhajraGeoJSON = $.ajax('http://localhost:8080/geoserver/coalrr/wfs',{
     service: 'WFS',
     version: '1.1.0',
     request: 'GetFeature',
-    typename: 'coalrr:jhajra_owner_join, coalrr:sonepur_owner_join',
+    typename: 'coalrr:kumarkhala_owner_join_modified, coalrr:benibandei_owner_join_modified',
     // CQL_FILTER: "column='text'",
     srsname: 'EPSG:4326',
     outputFormat: 'text/javascript',
@@ -160,7 +102,7 @@ function handleJson(data) {
         onEachFeature: function(feature, layer) { 
                                                           /////////////////////////////////////////////
             //  layer.on('click', function (e) {
-              // var uniq = feature.properties.unique_id;
+              // var uniq = feature.properties.u_plot_no;
               //var url = '';
               //var d = [];
            
@@ -193,23 +135,20 @@ function handleJson(data) {
           .setContent("<table>"+
             "<tr>"+"<td colspan = 2>"+"<center>"+"<b>"+"--PLOT INFORMATION--"+"</b>"+"</center>"+"</td>"+"</tr>"+
             "<tr>"+"<td colspan = 2>"+"<center>"+"------"+"</center>"+"</td>"+"</tr>"+
-            "<tr>"+"<td>"+"<b>"+"Unique Plot No:"+"</b>"+"</td>"+"<td>"+feature.properties.u_plot_no+"</td>"+"</tr>"+
-            "<tr>"+"<td>"+"<b>"+"Plot No:"+"</b>"+"</td>"+"<td>"+feature.properties.plot_num+"</td>"+"</tr>"+
-            "<tr>"+"<td>"+"<b>"+"JL No:"+"</b>"+"</td>"+"<td>"+feature.properties.jl_no+"</td>"+"</tr>"+
-            "<tr>"+"<td>"+"<b>"+"Land Type ID:"+"</b>"+"</td>"+"<td>"+feature.properties.landt_id+"</td>"+"</tr>"+
-            "<tr>"+"<td>"+"<b>"+"Mouza Name:"+"</b>"+"</td>"+"<td>"+feature.properties.mouza_name+"</td>"+"</tr>"+
+            "<tr>"+"<td>"+"<b>"+"Plot No:"+"</b>"+"</td>"+"<td>"+feature.properties.plot_no+"</td>"+"</tr>"+
             "<tr>"+"<td>"+"<b>"+"Owner Name:"+"</b>"+"</td>"+"<td>"+feature.properties.owner_name+"</td>"+"</tr>"+
+            "<tr>"+"<td>"+"<b>"+"Plot Type:"+"</b>"+"</td>"+"<td>"+feature.properties.plot_type+"</td>"+"</tr>"+
+            "<tr>"+"<td>"+"<b>"+"Mouza Name:"+"</b>"+"</td>"+"<td>"+feature.properties.mouza_name+"</td>"+"</tr>"+
             "<tr>"+"<td>"+"<b>"+"Khatian No:"+"</b>"+"</td>"+"<td>"+feature.properties.khatian_no+"</td>"+"</tr>"+
+            "<tr>"+"<td>"+"<b>"+"Total Plot Area:"+"</b>"+"</td>"+"<td>"+feature.properties.tot_area+"</td>"+"</tr>"+
             "<tr>"+"<td>"+"<b>"+"Shared Area:"+"</b>"+"</td>"+"<td>"+feature.properties.share_area+"</td>"+"</tr>"+
-            "<tr>"+"<td>"+"<b>"+"Land Shared:"+"</b>"+"</td>"+"<td>"+feature.properties.land_share+"</td>"+"</tr>"+
+            "<tr>"+"<td>"+"<b>"+"Shared Land:"+"</b>"+"</td>"+"<td>"+feature.properties.land_share+"</td>"+"</tr>"+
             "<tr>"+"<td>"+"<b>"+"Khatian Area Possessed:"+"</b>"+"</td>"+"<td>"+feature.properties.area_possd+"</td>"+"</tr>"+
-            "<tr>"+"<td>"+"<b>"+"Acquisition ID:"+"</b>"+"</td>"+"<td>"+feature.properties.acq_id+"</td>"+"</tr>"+
-            "<tr>"+"<td>"+"<b>"+"Land Use ID:"+"</b>"+"</td>"+"<td>"+feature.properties.land_use_id+"</td>"+"</tr>"+
-            "<tr>"+"<td>"+"<b>"+"Present Land Use:"+"</b>"+"</td>"+"<td>"+feature.properties.present_land_use+"</td>"+"</tr>"+
-            "<tr>"+"<td>"+"<b>"+"Possession Status:"+"</b>"+"</td>"+"<td>"+feature.properties.poss_is_full+"</td>"+"</tr>"+
-            "<tr>"+"<td>"+"<b>"+"Possession Reference No:"+"</b>"+"</td>"+"<td>"+feature.properties.poss_ref_no+"</td>"+"</tr>"+
-            "<tr>"+"<td>"+"<b>"+"Possession Date:"+"</b>"+"</td>"+"<td>"+feature.properties.poss_dt+"</td>"+"</tr>"+
-            "<tr>"+"<td>"+"<b>"+"Plot Area Possessed:"+"</b>"+"</td>"+"<td>"+feature.properties.poss_area+"</td>"+"</tr>"+
+            "<tr>"+"<td>"+"<b>"+"Present Land Use:"+"</b>"+"</td>"+"<td>"+feature.properties.present_la+"</td>"+"</tr>"+
+            // "<tr>"+"<td>"+"<b>"+"Possession Status:"+"</b>"+"</td>"+"<td>"+feature.properties.poss_is_full+"</td>"+"</tr>"+
+            // "<tr>"+"<td>"+"<b>"+"Possession Reference No:"+"</b>"+"</td>"+"<td>"+feature.properties.poss_ref_no+"</td>"+"</tr>"+
+            // "<tr>"+"<td>"+"<b>"+"Possession Date:"+"</b>"+"</td>"+"<td>"+feature.properties.poss_dt+"</td>"+"</tr>"+
+            // "<tr>"+"<td>"+"<b>"+"Plot Area Possessed:"+"</b>"+"</td>"+"<td>"+feature.properties.poss_area+"</td>"+"</tr>"+
           "</table>"
           );
         layer.bindPopup(popupContent);
@@ -217,62 +156,19 @@ function handleJson(data) {
     });
     wfsLayers.addTo(map);
   map.fitBounds(wfsLayers.getBounds());
-//leaflet layer control START
+
+//leaflet layer control: START
+//keeping the layer control within function handleJson
 var baseMaps = {
     'Open Street Map': osm
 };
 var overlayMaps = {
-    'WMSlayers': wmsLayers,
-    'WFSlayers': wfsLayers
+    'WMS Layers': wmsLayers,
+    'WFS Layers': wfsLayers
 };
 var LayerControl = L.control.layers(baseMaps, overlayMaps);
 LayerControl.addTo(map);
-//leaflet layer control END
+//leaflet layer control: END
 
-//Layer Switcher on left DIV
-const layerSwitcher = L.control.layers();
-layerSwitcher.onAdd = function() {
-    const div = L.DomUtil.create('div', 'layer-switcher');
-    // div.innerHTML = 
-    //     `<input type="checkbox" id="osm" checked>
-    //     <label for="osm">Open Street Map</label><br>
-    //     <input type="checkbox" id="wmsLayers">
-    //     <label for="wmsLayers">WMS layers</label><br>
-    //     <input type="checkbox" id="wfsLayers">
-    //     <label for="wfsLayers">WFS layers</label>`;
-    L.DomEvent.disableClickPropagation(div);
-    return div;
-};
-// layerSwitcher.addTo(map);
-//layers
-const OSM = L.layerGroup([osm]);
-const WMS = L.layerGroup([wmsLayers]);
-const WFS = L.layerGroup([wfsLayers]);
-// Add layers to the map
-OSM.addTo(map);
-WMS.addTo(map);
-WFS.addTo(map);
-// Toggle layers based on checkboxes
-document.getElementById('osm').addEventListener('change', function() {
-    if (this.checked) {
-        map.addLayer(OSM);
-    } else {
-        map.removeLayer(OSM);
-    }
-});
-document.getElementById('wmsLayers').addEventListener('change', function() {
-    if (this.checked) {
-        map.addLayer(WMS);
-    } else {
-        map.removeLayer(WMS);    
-      }
-});
-document.getElementById('wfsLayers').addEventListener('change', function() {
-  if (this.checked) {
-      map.addLayer(WFS);
-  } else {
-      map.removeLayer(WFS);
-  }
-});
+} //End of function HandleJson
 
-}
